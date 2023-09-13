702f06a09641667890463e11a1cd68a0669c30cb
Graham Watts <grahamcharleswatts@gmail.com>
2023-09-13 16:17:43 +0300

    Initial commit

---
+++ BearerTokens/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Claims;
+
+using Microsoft.AspNetCore.Authentication.BearerToken;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication()
+    .AddBearerToken();
+
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/login", () => Results.SignIn(new ClaimsPrincipal(new ClaimsIdentity(new[] { 
+    new Claim("iss", "my-app"), 
+    new Claim("sub", "test-user@my-app"),
+}, BearerTokenDefaults.AuthenticationScheme))));
+
+app.MapGet("/me", (HttpContext context) => context.User.Claims.Select(x => new { x.Type, x.Value }).ToArray())
+    .RequireAuthorization();
+
+app.Run();
